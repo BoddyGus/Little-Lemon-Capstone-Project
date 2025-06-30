c943d0d1e4cdd037827d832d25d9e87bb866d439
BoddyGus <yan.dulin06@gmail.com>
2025-06-30 11:13:09 +0300

    Updating App.js and Header section

--- capstone-project-app/src/App.js
+++ capstone-project-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from "Header.js"
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
---
+++ capstone-project-app/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav>
+                <ul>
+                    <li><Link to="header" smooth={true} duration={500}>Home</Link></li>
+                    <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+                    <li><Link to="reservations" smooth={true} duration={500}>Reservations</Link></li>
+                    <li><Link to="order" smooth={true} duration={500}>Order Online</Link></li>
+                    <li><Link to="login" smooth={true} duration={500}>Login</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
