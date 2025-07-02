9f7b3097cbf9e34ef4d1472ed93e71ad147fbef5
BoddyGus <yan.dulin06@gmail.com>
2025-07-02 11:29:37 +0300

    Updating Header.js

--- capstone-project-app/src/components/Header.js
+++ capstone-project-app/src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 const Header = () => {
     return (
-        <h1>This is the Header</h1>
+        <header>
+            <img src="LittleLemonLogo1.jpg" width="288" height="288"></img>
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>RESERVATIONS</li>
+                <li>ORDER ONLINE</li>
+                <li>LOGIN</li>
+            </ul>
+        </nav>
+        </header>
     )
 };
 export default Header;
