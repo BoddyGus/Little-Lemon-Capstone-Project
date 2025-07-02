2b16fb61277cb9e356006063fa8d78991b9d3d31
BoddyGus <yan.dulin06@gmail.com>
2025-07-02 11:14:08 +0300

    Making changes in App.js and Header.js

--- capstone-project-app/src/App.js
+++ capstone-project-app/src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
 import React from 'react';
-import Header from "./Header.js"
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+      <main>
+        <Header />
+      </main>
   );
 }
 
