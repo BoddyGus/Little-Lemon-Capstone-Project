8a413ec3980a636d4ab61851b78dd46985d9417f
BoddyGus <yan.dulin06@gmail.com>
2025-07-03 09:34:29 +0300

    Updating Header.js and App.js

--- capstone-project-app/src/App.js
+++ capstone-project-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from "./components/Header";
+import { ChakraProvider } from "@chakra-ui/react";
+
 function App() {
   return (
+    <ChakraProvider>
       <main>
         <Header />
       </main>
+    </ChakraProvider>
   );
 }
 
--- capstone-project-app/src/components/Header.js
+++ capstone-project-app/src/components/Header.js
@@ -1,18 +1,57 @@
 import React from "react";
-const Header = () => {
-    return (
-        <header>
-            <img src="LittleLemonLogo1.jpg" width="288" height="288"></img>
-        <nav>
-            <ul>
-                <li><a href = "jdflkasjfa">HOME</a></li>
-                <li><a href = "jdflkasjfa">ABOUT</a></li>
-                <li><a href = "jdflkasjfa">RESERVATIONS</a></li>
-                <li><a href = "jdflkasjfa">ORDER ONLINE</a></li>
-                <li><a href = "jdflkasjfa">LOGIN</a></li>
-            </ul>
-        </nav>
-        </header>
-    )
-};
+import { Box, HStack, Image, Link } from "@chakra-ui/react";
+
+const navLinks = [
+  { label: "HOME", href: "#home" },
+  { label: "ABOUT", href: "#about" },
+  { label: "RESERVATIONS", href: "#reservations" },
+  { label: "ORDER ONLINE", href: "#order-online" },
+  { label: "LOGIN", href: "#login" },
+];
+
+const Header = () => (
+  <Box
+    as="header"
+    position="fixed"
+    top={0}
+    left={0}
+    right={0}
+    bg="#18181b"
+    zIndex={1000}
+  >
+    <Box
+      maxW="1280px"
+      mx="auto"
+      color="white"
+      px={16}
+      py={4}
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Image
+        src="LittleLemonLogo1.jpg"
+        alt="Little Lemon Logo"
+        boxSize="72px"
+        objectFit="contain"
+      />
+      <nav>
+        <HStack spacing={8}>
+          {navLinks.map((link) => (
+            <Link
+              key={link.label}
+              href={link.href}
+              color="white"
+              fontWeight="bold"
+              _hover={{ color: "yellow.400", textDecoration: "none" }}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </HStack>
+      </nav>
+    </Box>
+  </Box>
+);
+
 export default Header;
